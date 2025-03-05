ab5a10b84a78c702073a356ec36f149271341617
zlew <1400377637@qq.com>
2025-03-05 18:40:28 +0800

    C语言代码练习

---
+++ 三位数的数位分离.c
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a,b,c,d;
+//	scanf("%d", &a);
+//	b = a % 10;
+//	c = (a / 10) % 10;
+//	d = a / 100;
+//	printf("%d %d %d\n",b, c, d);
+//	system("pause");
+//	return 0;
+//}
