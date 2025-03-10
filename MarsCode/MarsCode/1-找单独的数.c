8f7249c2d581117f598104e9ede10555f36d8c02
zlew <1400377637@qq.com>
2025-03-10 17:38:25 +0800

    C语言代码练习

---
+++ MarsCode/MarsCode/1-找单独的数.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+//
+//int solution(int cards[], int size) {
+//
+//    int ret = 0;
+//    for (int i = 0; i < size; i++)
+//    {
+//        ret ^= cards[i];
+//    }
+//    return ret;
+//}
+//
+//int main() {
+//
+//    int test1[] = { 1, 1, 2, 2, 3, 3, 4, 5, 5 };
+//
+//    if (solution(test1, 9) == 4)
+//    {
+//        printf("%d\n", solution(test1, 9));
+//    }
+//
+//    return 0;
+//}
